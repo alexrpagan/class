19699ca4ae40d8f86bc099be8e88916e65cc7d02
Alex Pagan <alexrpagan@gmail.com>
2013-12-02 13:05:35 -0500

    Refactor.

--- src/cpp/search/search.cpp
+++ src/cpp/search/search.cpp
@@ -11,7 +11,7 @@
 
 #include <algo/blast/api/sseqloc.hpp>
 #include <algo/blast/api/local_blast.hpp>
-#include <algo/blast/api/bl2seq.hpp> // added
+#include <algo/blast/api/bl2seq.hpp>
 #include <algo/blast/api/uniform_search.hpp>
 #include <algo/blast/api/blast_types.hpp>
 #include <algo/blast/api/blast_aux.hpp>
@@ -23,11 +23,9 @@
 #include <algo/blast/blastinput/blast_input.hpp>
 #include <algo/blast/blastinput/blast_fasta_input.hpp>
 
-#ifndef SKIP_DOXYGEN_PROCESSING
 USING_NCBI_SCOPE;
 USING_SCOPE(blast);
 USING_SCOPE(objects);
-#endif /* SKIP_DOXYGEN_PROCESSING */
 
 namespace {
 
@@ -36,10 +34,12 @@ namespace {
 
   const bool IS_PROTEIN = false;
 
-  const string BLAST_PROGRAM = "blastn";
-  const string INPUT_FLAG    = "in";
-  const string EVALUE_FLAG   = "evalue";
-  const string DB_FLAG       = "db";
+  const string COARSE_EVALUE_FLAG = "coarse_evalue";
+  const string BLAST_PROGRAM      = "blastn";
+  const string EVALUE_FLAG        = "evalue";
+  const string INPUT_FLAG         = "in";
+  const string DB_FLAG            = "db";
+
 }
 
 class CSearchApplication : public CNcbiApplication
@@ -48,14 +48,20 @@ private:
   virtual void Init(void);
   virtual int  Run(void);
   virtual void Exit(void);
+
+  CSearchResultSet RunBlast(string dbname, TSeqLocVector query_loc, CRef<CBlastOptionsHandle> opts);
+  void PrintErrorMessages(CSearchResults &queryResult);
+  void PrintQueryResult(CSearchResults &queryResult);
 };
 
-void CSearchApplication::Init(void)
+void
+CSearchApplication::Init(void)
 {
+
   auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
   arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
-                "Parallel blast over compressed genomes");
+                "Blast over bitvector-compressed genomes");
 
   arg_desc->AddKey
     (DB_FLAG, "Database",
@@ -68,14 +74,15 @@ void CSearchApplication::Init(void)
   arg_desc->AddDefaultKey(EVALUE_FLAG, "evalue",
     "E-value threshold for final hits", CArgDescriptions::eDouble, "1e-30");
 
-  // arg_desc->AddDefaultKey("coarse_evalue", "coarse_evalue",
-  // "E-value threshold for coarse search against uniques database", CArgDescriptions::eDouble, "1e-20");
+  arg_desc->AddDefaultKey(COARSE_EVALUE_FLAG, "CoarseEvalue",
+    "E-value threshold for coarse search against reference sequence", CArgDescriptions::eDouble, "1e-20");
 
   SetupArgDescriptions(arg_desc.release());
 
 }
 
-int CSearchApplication::Run(void)
+int
+CSearchApplication::Run(void)
 {
 
   const CArgs& args = GetArgs();
@@ -84,10 +91,9 @@ int CSearchApplication::Run(void)
 
   CRef<CBlastOptionsHandle> opts(CBlastOptionsFactory::Create(program));
 
-  if(args[EVALUE_FLAG].AsDouble()) {
-    opts->SetEvalueThreshold(args[EVALUE_FLAG].AsDouble());
+  if(args[COARSE_EVALUE_FLAG].AsDouble()) {
+    opts->SetEvalueThreshold(args[COARSE_EVALUE_FLAG].AsDouble());
   }
-
   opts->Validate();
 
   CRef<CObjectManager> objmgr = CObjectManager::GetInstance();
@@ -95,39 +101,73 @@ int CSearchApplication::Run(void)
     throw std::runtime_error("Could not initialize object manager");
   }
 
+  // this chunk of madness prepares the query file.
   SDataLoaderConfig dlconfig(IS_PROTEIN);
   CBlastInputSourceConfig iconfig(dlconfig);
   CBlastFastaInputSource fasta_input(args[INPUT_FLAG].AsInputFile(), iconfig);
   CScope scope(*objmgr);
-
   CBlastInput blast_input(&fasta_input);
   TSeqLocVector query_loc = blast_input.GetAllSeqLocs(scope);
 
-  const CSearchDatabase target_db(args[DB_FLAG].AsString(), CSearchDatabase::eBlastDbIsNucleotide);
-  CRef<IQueryFactory> query_factory(new CObjMgr_QueryFactory(query_loc));
-  CLocalBlast blaster(query_factory, opts, target_db);
-  CSearchResultSet results = *blaster.Run();
+  string dbname = args[DB_FLAG].AsString();
 
-  int num_results = 0;
-  for (int i = 0; i < results.GetNumResults(); i++) {
-    const list < CRef <CSeq_align> > &seqAlignList = results[i].GetSeqAlign()->Get();
-    num_results += seqAlignList.size();
+  CSearchResultSet results = RunBlast(dbname, query_loc, opts);
+  for (unsigned int i = 0; i < results.GetNumResults(); i++) {
+    CSearchResults &queryResult = results[i];
+    PrintErrorMessages(queryResult);
+    PrintQueryResult(queryResult);
   }
-  cout << "num_results: " << num_results << endl;
-  cout << endl;
 
   return SUCCESS;
 
 }
 
-void CSearchApplication::Exit(void)
+CSearchResultSet
+CSearchApplication::RunBlast(string dbname, TSeqLocVector query_loc, CRef<CBlastOptionsHandle> opts)
+{
+  const CSearchDatabase target_db(dbname, CSearchDatabase::eBlastDbIsNucleotide);
+  CRef<IQueryFactory> query_factory(new CObjMgr_QueryFactory(query_loc));
+  CLocalBlast blaster(query_factory, opts, target_db);
+  return *blaster.Run();
+}
+
+void
+CSearchApplication::PrintQueryResult(CSearchResults &queryResult)
+{
+  const list<CRef<CSeq_align> > &seqAlignList = queryResult.GetSeqAlign()->Get();
+  ITERATE(list <CRef<CSeq_align> >, seqAlignIter, seqAlignList) {
+    cout << "start-stop (target): id "
+      << (*seqAlignIter)->GetSeq_id(1).GetSeqIdString()
+      << " " << (*seqAlignIter)->GetSeqStart(1)
+      << "-" << (*seqAlignIter)->GetSeqStop(1) << endl;
+  }
+}
+
+void
+CSearchApplication::PrintErrorMessages(CSearchResults &queryResult) {
+    // print any error messages
+    TQueryMessages messages = queryResult.GetErrors(eBlastSevWarning);
+    if (messages.size() > 0) {
+      CConstRef<CSeq_id> seq_id = queryResult.GetSeqId();
+      if (seq_id.NotEmpty()) {
+        cerr << "ID: " << seq_id->AsFastaString() << endl;
+      } else {
+        cerr << "ID: " << "Unknown" << endl;
+      }
+      ITERATE(vector<CRef<CSearchMessage> >, it, messages) {
+        cerr << (*it)->GetMessage() << endl;
+      }
+    }
+}
+
+void
+CSearchApplication::Exit(void)
 {
   SetDiagStream(0);
 }
 
-#ifndef SKIP_DOXYGEN_PROCESSING
-int main(int argc, const char* argv[])
+int
+main(int argc, const char* argv[])
 {
   return CSearchApplication().AppMain(argc, argv, 0, eDS_Default, 0);
 }
-#endif /* SKIP_DOXYGEN_PROCESSING */
