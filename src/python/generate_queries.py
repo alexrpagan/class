f9043e8fc73b4760fd58ca6b8e32ec871150b49e
Alex Pagan <alexrpagan@gmail.com>
2013-12-04 00:24:40 -0500

    script to generate workload

---
+++ src/python/generate_queries.py
@@ -0,0 +1,44 @@
+import random
+
+REFERENCE   = '/data/apagan/tgc/REFERENCE/chr22.fa'
+NUM_QUERIES = 10000
+MIN_LENGTH  = 5
+MAX_LENGTH  = 200
+SEED        = 0
+
+random.seed(SEED)
+
+BASES = ['A', 'C', 'G', 'T']
+
+def main():
+    queries = []
+    origins = []
+    ref = ""
+    seen_defline = False
+    with open(REFERENCE, 'r') as f:
+        for line in f:
+            if not seen_defline:
+                seen_defline = True
+                continue
+            ref += line.strip()
+
+    for _ in range(NUM_QUERIES):
+        start   = random.randint(0, len(ref) - MAX_LENGTH - 1)
+        length  = random.randint(MIN_LENGTH, MAX_LENGTH)
+        replace = random.random()
+        ref_seq = ref[start:start+length]
+        query = ""
+        for c in ref_seq:
+            if random.random() < replace:
+                query += random.choice(BASES)
+            else:
+                query += c
+        queries.append(query)
+        origins.append((start, replace,))
+
+    for q, o in zip(queries, origins):
+        print ">%s|origin: %d|replace:%f" % (REFERENCE, o[0], o[1])
+        print q
+
+if __name__ == "__main__":
+    main()
