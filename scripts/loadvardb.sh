c117a95ee7e584e055f902cc7efc85dff71519b7
Alex Pagan <alexrpagan@gmail.com>
2013-12-03 23:51:37 -0500

    Removed old code

--- scripts/loadvardb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VARIANTDB=/data/apagan/variantdb
-REDISDIR=/home/apagan/code/redis-2.6.16/src
-
-for file in "$VARIANTDB"/*.vd; do
-    echo "Loading $file into redis"
-    python redis_inserts_for_vardb.py $file | "$REDISDIR/"redis-cli --pipe
-done
